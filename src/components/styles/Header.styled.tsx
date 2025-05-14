1d3bc56c10fbd4f367a9d505c88fb8fc4af92c97
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-14 10:05:01 -0300

    Improve button style

--- src/components/styles/Header.styled.tsx
+++ src/components/styles/Header.styled.tsx
@@ -10,10 +10,12 @@ export const HeaderStyled = styled.header`
 
   button {
     --size: 3.125rem;
+    background-color: var(--neutral-100);
     border: none;
-    width: var(--size);
-    height: var(--size);
     border-radius: 0.75rem;
+    cursor: pointer;
+    height: var(--size);
+    width: var(--size);
 
     & img {
       display: block;
