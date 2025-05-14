78800cc7f60a54de86a647483e506fe0be8cb2bb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-14 11:08:30 -0300

    Create generic styled button

---
+++ src/components/styles/Button.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button`
+  border-radius: 22px;
+  border: none;
+  background-color: var(--neutral-0);
+  color: var(--neutral-900);
+  font-size: 20px;
+  padding: 8px 20px;
+`;
