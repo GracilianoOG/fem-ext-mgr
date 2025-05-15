08cd172a819433e47fa3b50a8fab487998e04e1b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-15 11:26:49 -0300

    Add extensions filters component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,9 @@
-import { ButtonStyled } from "./styles/Button.styled";
-import { ExtensionsFiltersStyled } from "./styles/ExtensionsFilters.styled";
-import { FiltersContainerStyled } from "./styles/FiltersContainer.styled";
-import { TitleStyled } from "./styles/Title.styled";
+import ExtensionsFilters from "./ExtensionsFilters";
 
 const Main = () => {
   return (
     <main>
-      <ExtensionsFiltersStyled>
-        <TitleStyled>Extensions List</TitleStyled>
-        <FiltersContainerStyled>
-          <ButtonStyled>All</ButtonStyled>
-          <ButtonStyled>Active</ButtonStyled>
-          <ButtonStyled>Inactive</ButtonStyled>
-        </FiltersContainerStyled>
-      </ExtensionsFiltersStyled>
+      <ExtensionsFilters />
     </main>
   );
 };
