91394b83bd3c6c1a3e7fc807873bbb3d18fa4adb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-15 11:54:26 -0300

    Get filter values from filter types

--- src/components/ExtensionsFilters.tsx
+++ src/components/ExtensionsFilters.tsx
@@ -3,29 +3,30 @@ import { ButtonStyled } from "./styles/Button.styled";
 import { ExtensionsFiltersStyled } from "./styles/ExtensionsFilters.styled";
 import { FiltersContainerStyled } from "./styles/FiltersContainer.styled";
 import { TitleStyled } from "./styles/Title.styled";
+import { FilterTypes } from "../enums/FilterTypes";
 
 const ExtensionsFilters = () => {
-  const [selected, setSelected] = useState<string>("all");
+  const [selected, setSelected] = useState<string>(FilterTypes.ALL);
 
   return (
     <ExtensionsFiltersStyled>
       <TitleStyled>Extensions List</TitleStyled>
       <FiltersContainerStyled>
         <ButtonStyled
-          $selected={selected === "all"}
-          onClick={() => setSelected("all")}
+          $selected={selected === FilterTypes.ALL}
+          onClick={() => setSelected(FilterTypes.ALL)}
         >
           All
         </ButtonStyled>
         <ButtonStyled
-          $selected={selected === "active"}
-          onClick={() => setSelected("active")}
+          $selected={selected === FilterTypes.ACTIVE}
+          onClick={() => setSelected(FilterTypes.ACTIVE)}
         >
           Active
         </ButtonStyled>
         <ButtonStyled
-          $selected={selected === "inactive"}
-          onClick={() => setSelected("inactive")}
+          $selected={selected === FilterTypes.INACTIVE}
+          onClick={() => setSelected(FilterTypes.INACTIVE)}
         >
           Inactive
         </ButtonStyled>
