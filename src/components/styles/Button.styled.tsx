006341bcff296bd78791e9115a30318468e566bc
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-15 11:27:13 -0300

    Add styles if  is true

--- src/components/styles/Button.styled.tsx
+++ src/components/styles/Button.styled.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const ButtonStyled = styled.button<{ $selected?: boolean }>`
   border-radius: 22px;
@@ -7,4 +7,11 @@ export const ButtonStyled = styled.button<{ $selected?: boolean }>`
   color: var(--neutral-900);
   font-size: 20px;
   padding: 8px 20px;
+
+  ${({ $selected }) =>
+    $selected &&
+    css`
+      background-color: var(--red-700);
+      color: var(--neutral-0);
+    `}
 `;
