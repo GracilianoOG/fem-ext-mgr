885c20345e19cd94892ba749436bbc57f719359b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-16 13:59:49 -0300

    Add card list to main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,34 @@
 import ExtensionsFilters from "./ExtensionsFilters";
+import {
+  CardListContentStyled,
+  CardListItemStyled,
+  CardListStyled,
+} from "./styles/CardList.styled";
+import img from "../assets/images/logos/logo-devlens.svg";
+import { RemoveButtonStyled } from "./styles/Button.styled";
 
 const Main = () => {
   return (
     <main>
       <ExtensionsFilters />
+      <CardListStyled>
+        <CardListItemStyled>
+          <CardListContentStyled>
+            <div>
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <h2>DevLens</h2>
+              <p>
+                Quickly inspect page layouts and visualize element boundaries.
+              </p>
+            </div>
+          </CardListContentStyled>
+          <div>
+            <RemoveButtonStyled>Remove</RemoveButtonStyled>
+          </div>
+        </CardListItemStyled>
+      </CardListStyled>
     </main>
   );
 };
