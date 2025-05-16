5d0477a557e169666beb6faa80d1323dc8c8c4ec
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-16 13:58:47 -0300

    Create card list component

---
+++ src/components/styles/CardList.styled.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const CardListStyled = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+`;
+
+export const CardListItemStyled = styled.li`
+  background-color: white;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  list-style: none;
+  min-height: 200px;
+  padding: 20px;
+  width: 100%;
+`;
+
+export const CardListContentStyled = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  img {
+    display: block;
+  }
+
+  h2 {
+    color: var(--neutral-900);
+    font-size: 20px;
+    margin-bottom: 5px;
+  }
+
+  p {
+    color: var(--neutral-700);
+    font-size: 15px;
+    line-height: 1.5;
+  }
+`;
