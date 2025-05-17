362ae81a84c9255f9731c199b1935bf693d78962
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-17 18:02:38 -0300

    Remove styles related to CardItem

--- src/components/styles/CardList.styled.tsx
+++ src/components/styles/CardList.styled.tsx
@@ -14,36 +14,3 @@ export const CardListStyled = styled.ul`
     grid-template-columns: repeat(3, 1fr);
   }
 `;
-
-export const CardListItemStyled = styled.li`
-  background-color: white;
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  list-style: none;
-  min-height: 200px;
-  padding: 20px;
-  width: 100%;
-`;
-
-export const CardListContentStyled = styled.div`
-  display: flex;
-  gap: 1rem;
-
-  img {
-    display: block;
-  }
-
-  h2 {
-    color: var(--neutral-900);
-    font-size: 20px;
-    margin-bottom: 5px;
-  }
-
-  p {
-    color: var(--neutral-700);
-    font-size: 15px;
-    line-height: 1.5;
-  }
-`;
