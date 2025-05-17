bac075b7b08f4a3a0fc571c9bcacdf313e2e5cac
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-17 10:27:02 -0300

    Change exntensions filters layout on bigger screen

--- src/components/styles/ExtensionsFilters.styled.tsx
+++ src/components/styles/ExtensionsFilters.styled.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
 export const ExtensionsFiltersStyled = styled.div`
+  align-items: center;
   display: flex;
   flex-direction: column;
   gap: 20px;
+  justify-content: center;
   margin-bottom: 42px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
