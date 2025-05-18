e02fc10fbf8f31f1b8628a2fa1b69e887bc07ea1
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-18 20:39:38 -0300

    Create interface for extension toggler props

--- src/components/ExtensionToggler.tsx
+++ src/components/ExtensionToggler.tsx
@@ -1,13 +1,12 @@
 import { ExtensionTogglerStyled } from "./styles/ExtensionToggler.styled";
 import { useExtensions } from "../hooks/useExtensions";
 
-const ExtensionToggler = ({
-  isActive,
-  extId,
-}: {
+interface ExtensionTogglerProps {
   isActive: boolean;
   extId: string;
-}) => {
+}
+
+const ExtensionToggler = ({ isActive, extId }: ExtensionTogglerProps) => {
   const { setExtensions } = useExtensions();
 
   const handleClick = () =>
