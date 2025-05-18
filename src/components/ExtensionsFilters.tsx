36fe6c897c4280e06e585a8700b02d205ddb440f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-17 21:00:05 -0300

    Replace styled components with FilterButton

--- src/components/ExtensionsFilters.tsx
+++ src/components/ExtensionsFilters.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { ButtonStyled } from "./styles/Button.styled";
 import { ExtensionsFiltersStyled } from "./styles/ExtensionsFilters.styled";
 import { FiltersContainerStyled } from "./styles/FiltersContainer.styled";
 import { TitleStyled } from "./styles/Title.styled";
 import { FilterTypes } from "../enums/FilterTypes";
+import FilterButton from "./FilterButton";
 
 const ExtensionsFilters = () => {
   const [selected, setSelected] = useState<string>(FilterTypes.ALL);
@@ -12,24 +12,21 @@ const ExtensionsFilters = () => {
     <ExtensionsFiltersStyled>
       <TitleStyled>Extensions List</TitleStyled>
       <FiltersContainerStyled>
-        <ButtonStyled
-          $selected={selected === FilterTypes.ALL}
-          onClick={() => setSelected(FilterTypes.ALL)}
-        >
-          All
-        </ButtonStyled>
-        <ButtonStyled
-          $selected={selected === FilterTypes.ACTIVE}
-          onClick={() => setSelected(FilterTypes.ACTIVE)}
-        >
-          Active
-        </ButtonStyled>
-        <ButtonStyled
-          $selected={selected === FilterTypes.INACTIVE}
-          onClick={() => setSelected(FilterTypes.INACTIVE)}
-        >
-          Inactive
-        </ButtonStyled>
+        <FilterButton
+          selected={selected}
+          setSelected={setSelected}
+          btnFilter={FilterTypes.ALL}
+        />
+        <FilterButton
+          selected={selected}
+          setSelected={setSelected}
+          btnFilter={FilterTypes.ACTIVE}
+        />
+        <FilterButton
+          selected={selected}
+          setSelected={setSelected}
+          btnFilter={FilterTypes.INACTIVE}
+        />
       </FiltersContainerStyled>
     </ExtensionsFiltersStyled>
   );
