5409ae81ddc80ebc185b860dcd307c60ad28c0c8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-18 12:41:16 -0300

    Create filter state

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,15 @@
 import ExtensionsFilters from "./ExtensionsFilters";
 import CardList from "./CardList";
+import { useState } from "react";
+import { FilterTypes } from "../enums/FilterTypes";
 
 const Main = () => {
+  const [filter, setFilter] = useState<string>(FilterTypes.ALL);
+
   return (
     <main>
-      <ExtensionsFilters />
-      <CardList />
+      <ExtensionsFilters filter={filter} setFilter={setFilter} />
+      <CardList filter={filter} />
     </main>
   );
 };
