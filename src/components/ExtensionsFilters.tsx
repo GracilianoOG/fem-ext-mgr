7141f9001f9220068b09aa3654f689e633d96aed
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-18 12:41:34 -0300

    Use filter state from props

--- src/components/ExtensionsFilters.tsx
+++ src/components/ExtensionsFilters.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
 import { ExtensionsFiltersStyled } from "./styles/ExtensionsFilters.styled";
 import { FiltersContainerStyled } from "./styles/FiltersContainer.styled";
 import { TitleStyled } from "./styles/Title.styled";
 import { FilterTypes } from "../enums/FilterTypes";
 import FilterButton from "./FilterButton";
 
-const ExtensionsFilters = () => {
-  const [filter, setFilter] = useState<string>(FilterTypes.ALL);
-
+const ExtensionsFilters = ({
+  filter,
+  setFilter,
+}: {
+  filter: string;
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+}) => {
   return (
     <ExtensionsFiltersStyled>
       <TitleStyled>Extensions List</TitleStyled>
