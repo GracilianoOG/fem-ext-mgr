6f22c9d50093d72c97befc2aa8da312594489ff6
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-19 14:51:23 -0300

    Add hover effect for header theme button

--- src/components/styles/Header.styled.tsx
+++ src/components/styles/Header.styled.tsx
@@ -19,11 +19,16 @@ export const HeaderStyled = styled.header`
     border-radius: 0.75rem;
     cursor: pointer;
     height: var(--size);
+    transition: background-color 0.25s;
     width: var(--size);
 
     & img {
       display: block;
       margin: auto;
+    }
+
+    &:hover {
+      background-color: var(--neutral-300);
     }
   }
 
