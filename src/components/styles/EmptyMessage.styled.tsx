77d969d896131281d79acf487b5bd255cbf91f39
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-19 15:11:36 -0300

    Create empty message styled

---
+++ src/components/styles/EmptyMessage.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const EmptyMessageStyled = styled.p`
+  color: var(--neutral-900);
+  border: 2px dashed currentColor;
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-transform: capitalize;
+  text-align: center;
+  padding: 1.5rem 0;
+`;
