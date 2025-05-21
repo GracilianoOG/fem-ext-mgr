6101926fa44e97d9d34e7fc6d27d5c2ad5493ccb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-21 19:42:10 -0300

    Add round borders to empty message

--- src/components/styles/EmptyMessage.styled.tsx
+++ src/components/styles/EmptyMessage.styled.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const EmptyMessageStyled = styled.p`
   color: ${({ theme }) => theme.colors.emptyMsgColor};
   border: 2px dashed currentColor;
+  border-radius: 0.75rem;
   font-size: 1.5rem;
   font-weight: 700;
   text-transform: capitalize;
