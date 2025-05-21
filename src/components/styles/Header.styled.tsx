8b8563addb7c4e67ff78b95a8722c6dc9f1d159f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-21 19:53:57 -0300

    Remove styled button

--- src/components/styles/Header.styled.tsx
+++ src/components/styles/Header.styled.tsx
@@ -12,26 +12,6 @@ export const HeaderStyled = styled.header`
   margin-bottom: 2.375rem;
   padding: 0.438rem 0.688rem;
 
-  button {
-    --size: 3.125rem;
-    background-color: ${({ theme }) => theme.colors.themeBtnBgColor};
-    border: none;
-    border-radius: 0.75rem;
-    cursor: pointer;
-    height: var(--size);
-    transition: background-color 0.25s;
-    width: var(--size);
-
-    & img {
-      display: block;
-      margin: auto;
-    }
-
-    &:hover {
-      background-color: ${({ theme }) => theme.colors.themeBtnBgColorHover};
-    }
-  }
-
   ${mediaQueries.laptop} {
     border-radius: 1.125rem;
     padding: 0.625rem 1rem;
