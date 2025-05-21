f80d1de21102bf04642c41d7102e55f9cb70ac74
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-21 19:37:37 -0300

    Get empty message color from theme

--- src/components/styles/EmptyMessage.styled.tsx
+++ src/components/styles/EmptyMessage.styled.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const EmptyMessageStyled = styled.p`
-  color: var(--neutral-900);
+  color: ${({ theme }) => theme.colors.emptyMsgColor};
   border: 2px dashed currentColor;
   font-size: 1.5rem;
   font-weight: 700;
