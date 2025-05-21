e3f1637df428f2b6b1ce37f9bb9fd94ea306552b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-21 19:53:38 -0300

    Replace button with ThemeTogglerStyled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import logoDark from "/logos/logo-dark.svg";
 import moonIcon from "../assets/images/icons/icon-moon.svg";
 import sunIcon from "../assets/images/icons/icon-sun.svg";
 import { HeaderStyled } from "./styles/Header.styled";
+import { ThemeTogglerStyled } from "./styles/ThemeToggler.styled";
 
 interface HeaderProps {
   theme: string;
@@ -13,9 +14,9 @@ const Header = ({ theme, toggleTheme }: HeaderProps) => {
   return (
     <HeaderStyled>
       <img src={theme === "light" ? logo : logoDark} alt="Extensions" />
-      <button onClick={toggleTheme}>
+      <ThemeTogglerStyled onClick={toggleTheme}>
         <img src={theme === "light" ? moonIcon : sunIcon} alt="" />
-      </button>
+      </ThemeTogglerStyled>
     </HeaderStyled>
   );
 };
