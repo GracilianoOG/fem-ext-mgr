7d375fe837845fc8251984d37fce72e7a79b475b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-24 20:18:11 -0300

    Update live region when deleting an extension

--- src/components/RemoveButton.tsx
+++ src/components/RemoveButton.tsx
@@ -1,13 +1,17 @@
 import { useExtensions } from "../hooks/useExtensions";
+import { useLiveRegion } from "../hooks/useLiveRegion";
 import { RemoveButtonStyled } from "./styles/Button.styled";
 
 const RemoveButton = ({ name, extId }: { name: string; extId: string }) => {
   const { setExtensions } = useExtensions();
+  const { updateLiveRegion } = useLiveRegion();
 
-  const handleClick = () =>
+  const handleClick = () => {
     setExtensions(prevExtensions =>
       prevExtensions.filter(ext => ext.id !== extId)
     );
+    updateLiveRegion(`Removed ${name} extension.`);
+  };
 
   return (
     <RemoveButtonStyled onClick={handleClick} aria-label={`Remove ${name}`}>
