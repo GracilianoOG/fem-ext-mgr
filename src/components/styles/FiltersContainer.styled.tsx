7327f59c374e0db829a7b2c580a4def7519b4ef7
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-24 11:13:56 -0300

    Change container to ul element

--- src/components/styles/FiltersContainer.styled.tsx
+++ src/components/styles/FiltersContainer.styled.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
-export const FiltersContainerStyled = styled.div`
+export const FiltersContainerStyled = styled.ul`
   display: flex;
   gap: 0.75rem;
   justify-content: center;
+  list-style: none;
 `;
