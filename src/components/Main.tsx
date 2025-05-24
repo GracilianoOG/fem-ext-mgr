438556ef9eae28f651fd25b9088484391228b180
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-24 20:17:21 -0300

    Add live region provider

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { FilterTypes } from "../enums/FilterTypes";
 import ExtensionProvider from "../providers/ExtensionProvider";
 import { MainStyled } from "./styles/Main.styled";
+import LiveRegionProvider from "../providers/LiveRegionProvider";
 
 const Main = () => {
   const [filter, setFilter] = useState<string>(FilterTypes.ALL);
@@ -12,7 +13,9 @@ const Main = () => {
     <MainStyled>
       <ExtensionsFilters filter={filter} setFilter={setFilter} />
       <ExtensionProvider>
-        <CardList filter={filter} />
+        <LiveRegionProvider>
+          <CardList filter={filter} />
+        </LiveRegionProvider>
       </ExtensionProvider>
     </MainStyled>
   );
