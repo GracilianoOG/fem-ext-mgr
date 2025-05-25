b839cb8f1627e1c9041e58e5b3d2f76bf047f500
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-25 11:31:55 -0300

    Move extension context interface

--- src/contexts/ExtensionContext.tsx
+++ src/contexts/ExtensionContext.tsx
@@ -1,5 +1,10 @@
 import { createContext } from "react";
-import type { IExtensionContext } from "../interfaces/IExtensionContext";
+import type { CardItemProps } from "../interfaces/CardItemProps";
+
+interface IExtensionContext {
+  extensions: CardItemProps[];
+  setExtensions: React.Dispatch<React.SetStateAction<CardItemProps[]>>;
+}
 
 const defaultContextValue: IExtensionContext = {
   extensions: [],
--- src/interfaces/IExtensionContext.ts
+++
@@ -1,6 +0,0 @@
-import type { CardItemProps } from "./CardItemProps";
-
-export interface IExtensionContext {
-  extensions: CardItemProps[];
-  setExtensions: React.Dispatch<React.SetStateAction<CardItemProps[]>>;
-}
