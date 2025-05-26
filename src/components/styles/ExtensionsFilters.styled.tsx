73c083e0b29f3e3662deee552bc625cc7ac706c3
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-25 22:03:07 -0300

    Change div to section

--- src/components/styles/ExtensionsFilters.styled.tsx
+++ src/components/styles/ExtensionsFilters.styled.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { mediaQueries } from "../../utils/mediaQueries";
 
-export const ExtensionsFiltersStyled = styled.div`
+export const ExtensionsFiltersStyled = styled.section`
   align-items: center;
   display: flex;
   flex-direction: column;
