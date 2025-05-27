da63027c14f2a120885493f245879e4cf597808f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-27 20:38:24 -0300

    Throw an error if the context is not set

--- src/hooks/useExtensions.tsx
+++ src/hooks/useExtensions.tsx
@@ -1,4 +1,12 @@
 import { useContext } from "react";
 import { ExtensionContext } from "../contexts/ExtensionContext";
 
-export const useExtensions = () => useContext(ExtensionContext);
+export const useExtensions = () => {
+  const context = useContext(ExtensionContext);
+
+  if (!context) {
+    throw new Error("useExtensions must be used within an ExtensionProvider!");
+  }
+
+  return context;
+};
