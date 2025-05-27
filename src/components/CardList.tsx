441aa97da02d4c8d17f8cd2954e368c16940efba
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-27 16:30:40 -0300

    Cache extension filtering
    - Wrap getExtensions with useCallback.
    - Wrap visible extensions with useMemo to avoid re-doing the operation.

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -3,21 +3,28 @@ import { CardListStyled } from "./styles/CardList.styled";
 import { FilterTypes } from "../enums/FilterTypes";
 import { useExtensions } from "../hooks/useExtensions";
 import { EmptyMessageStyled } from "./styles/EmptyMessage.styled";
+import { useCallback, useMemo } from "react";
 
 const CardList = ({ filter }: { filter: string }) => {
   const { extensions } = useExtensions();
 
-  const getExtensions = () => {
-    if (filter === FilterTypes.ALL) {
-      return extensions;
-    } else if (filter === FilterTypes.ACTIVE) {
-      return extensions.filter(ext => ext.isActive);
-    } else {
-      return extensions.filter(ext => !ext.isActive);
-    }
-  };
+  const getExtensions = useCallback(
+    (filter: string) => {
+      if (filter === FilterTypes.ALL) {
+        return extensions;
+      } else if (filter === FilterTypes.ACTIVE) {
+        return extensions.filter(ext => ext.isActive);
+      } else {
+        return extensions.filter(ext => !ext.isActive);
+      }
+    },
+    [extensions]
+  );
 
-  const visibleExtensions = getExtensions();
+  const visibleExtensions = useMemo(
+    () => getExtensions(filter),
+    [getExtensions, filter]
+  );
 
   return visibleExtensions.length ? (
     <CardListStyled>
