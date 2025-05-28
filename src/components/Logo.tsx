1178235f3ac85627852a289e57c54cbe54518e66
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-27 22:26:36 -0300

    Set default width and height for main logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,14 @@ import logo from "/logos/logo.svg";
 import logoDark from "/logos/logo-dark.svg";
 
 const Logo = ({ theme }: { theme: string }) => {
-  return <img src={theme === "light" ? logo : logoDark} alt="Extensions" />;
+  return (
+    <img
+      src={theme === "light" ? logo : logoDark}
+      alt="Extensions"
+      width={179}
+      height={41}
+    />
+  );
 };
 
 export default Logo;
