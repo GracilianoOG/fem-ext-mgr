ce23fe5f8f9d81a70e80869833c987b9c4ff4fbd
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-12 09:24:39 -0300

    Get theme and toggler from useThemes

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,13 +1,11 @@
 import moonIcon from "../assets/images/icons/icon-moon.svg";
 import sunIcon from "../assets/images/icons/icon-sun.svg";
+import { useThemes } from "../hooks/useThemes";
 import { ThemeTogglerStyled } from "./styles/ThemeToggler.styled";
 
-interface ThemeTogglerProps {
-  theme: string;
-  toggleTheme: () => void;
-}
+const ThemeToggler = () => {
+  const { theme, toggleTheme } = useThemes();
 
-const ThemeToggler = ({ theme, toggleTheme }: ThemeTogglerProps) => {
   return (
     <ThemeTogglerStyled
       onClick={toggleTheme}
