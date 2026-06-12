e2d7fc2ba19ac9cce8c500046258789a89e54e4c
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-12 09:24:57 -0300

    Remove passed props from theme toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,12 @@
 import { HeaderStyled } from "./styles/Header.styled";
 import Logo from "./Logo";
 import ThemeToggler from "./ThemeToggler";
-import { useThemes } from "../hooks/useThemes";
 
 const Header = () => {
-  const { theme, toggleTheme } = useThemes();
-
   return (
     <HeaderStyled>
       <Logo />
-      <ThemeToggler theme={theme} toggleTheme={toggleTheme} />
+      <ThemeToggler />
     </HeaderStyled>
   );
 };
