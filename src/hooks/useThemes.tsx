a7497d2a8952e72d94206e394e86dd820313bc2b
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-12 09:08:56 -0300

    Return theme context from useThemes

--- src/hooks/useThemes.tsx
+++ src/hooks/useThemes.tsx
@@ -1,29 +1,12 @@
-import { useEffect, useState } from "react";
-
-type ThemeType = "light" | "dark";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 export const useThemes = () => {
-  const [theme, setTheme] = useState<ThemeType>(() => {
-    const storedTheme = localStorage.getItem("theme") as ThemeType;
+  const context = useContext(ThemeContext);
 
-    if (storedTheme) {
-      return storedTheme;
-    }
+  if (!context) {
+    throw new Error("useThemes must be used within a ThemeProvider!");
+  }
 
-    const prefersDarkTheme = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
-
-    return prefersDarkTheme ? "dark" : "light";
-  });
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  return [theme, toggleTheme];
+  return context;
 };
