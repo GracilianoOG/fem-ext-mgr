5a04758608b534c5ad9c9dce12a8557b9816f18c
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-12 09:18:59 -0300

    Remove passed props from Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { GlobalStyles } from "./components/styles/GlobalStyles.styled";
-import { useThemes } from "./hooks/useThemes";
 import Footer from "./components/Footer";
 
 function App() {
-  const { theme, toggleTheme } = useThemes();
-
   return (
     <>
       <GlobalStyles />
-      <Header theme={theme} toggleTheme={toggleTheme} />
+      <Header />
       <Main />
       <Footer />
     </>
