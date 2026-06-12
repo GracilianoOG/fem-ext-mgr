bd72c66e66c76925cb1df4c2f25aa274bfbd2dec
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-12 15:10:07 -0300

    Style confirm modal

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,5 +1,44 @@
 import { useState } from "react";
 import { BorderButtonStyled, DangerButtonStyled } from "./styles/Button.styled";
+import styled from "styled-components";
+
+const ConfirmModalStyled = styled.div`
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  inset: 0;
+  justify-content: center;
+  position: fixed;
+  z-index: 999;
+
+  h2 {
+    color: ${({ theme }) => theme.colors.cardTitleColor};
+    line-height: 2;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.cardDescColor};
+    line-height: 1.5;
+    margin-bottom: 2rem;
+  }
+
+  & > div {
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    background-color: ${({ theme }) => theme.colors.cardBgColor};
+    border-radius: 1.25rem;
+    padding: 1.25rem;
+    width: min(100%, 20rem);
+  }
+`;
+
+const ConfirmModalButtonsStyled = styled.div`
+  align-items: center;
+  justify-content: flex-end;
+  display: flex;
+  gap: 8px;
+`;
 
 export const ConfirmModal = () => {
   const [isModal, setIsModal] = useState(true);
@@ -9,17 +48,17 @@ export const ConfirmModal = () => {
   }
 
   return (
-    <div>
+    <ConfirmModalStyled>
       <div>
         <h2>Confirm Modal</h2>
         <p>Press the confirm button to confirm.</p>
-        <div>
+        <ConfirmModalButtonsStyled>
           <BorderButtonStyled onClick={() => setIsModal(false)}>
             Cancel
           </BorderButtonStyled>
           <DangerButtonStyled>Remove</DangerButtonStyled>
-        </div>
+        </ConfirmModalButtonsStyled>
       </div>
-    </div>
+    </ConfirmModalStyled>
   );
 };
