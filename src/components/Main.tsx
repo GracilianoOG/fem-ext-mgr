4ac497820825fe61cff1ef6e2e843d1dbafb4bb9
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-14 11:55:33 -0300

    Wrap main content with filter provider

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,26 +1,25 @@
 import ExtensionsFilters from "./ExtensionsFilters";
 import CardList from "./CardList";
-import { useState } from "react";
-import { FilterTypes } from "../enums/FilterTypes";
 import ExtensionProvider from "../providers/ExtensionProvider";
 import { MainStyled } from "./styles/Main.styled";
 import LiveRegionProvider from "../providers/LiveRegionProvider";
 import { VisuallyHidden } from "./styles/VisuallyHidden.styled";
+import FilterProvider from "../providers/FilterProvider";
 
 const Main = () => {
-  const [filter, setFilter] = useState<string>(FilterTypes.ALL);
-
   return (
     <MainStyled>
-      <ExtensionsFilters filter={filter} setFilter={setFilter} />
-      <ExtensionProvider>
-        <LiveRegionProvider>
-          <section>
-            <VisuallyHidden as="h2">Available extensions</VisuallyHidden>
-            <CardList filter={filter} />
-          </section>
-        </LiveRegionProvider>
-      </ExtensionProvider>
+      <FilterProvider>
+        <ExtensionsFilters />
+        <ExtensionProvider>
+          <LiveRegionProvider>
+            <section>
+              <VisuallyHidden as="h2">Available extensions</VisuallyHidden>
+              <CardList />
+            </section>
+          </LiveRegionProvider>
+        </ExtensionProvider>
+      </FilterProvider>
     </MainStyled>
   );
 };
