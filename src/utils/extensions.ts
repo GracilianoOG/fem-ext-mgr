84117f4f097b0fd8ecfdb1db6281898af4f66e7b
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-14 11:23:37 -0300

    Create filterExtensions function

---
+++ src/utils/extensions.ts
@@ -0,0 +1,15 @@
+import { FilterTypes } from "../enums/FilterTypes";
+import type { CardItemProps } from "../interfaces/CardItemProps";
+
+export const filterExtensions = (
+  filter: string,
+  extensions: CardItemProps[],
+) => {
+  if (filter === FilterTypes.ALL) {
+    return extensions;
+  } else if (filter === FilterTypes.ACTIVE) {
+    return extensions.filter((ext) => ext.isActive);
+  } else {
+    return extensions.filter((ext) => !ext.isActive);
+  }
+};
