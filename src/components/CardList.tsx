0a7d8f6ff486e5f48b1a5e21f1786261197ce8f1
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-14 11:27:32 -0300

    Remove useMemo from visibleExtensions
    This value will constantly change, there is no need memoize it.

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,16 +2,11 @@ import CardItem from "./CardItem";
 import { CardListStyled } from "./styles/CardList.styled";
 import { useExtensions } from "../hooks/useExtensions";
 import { EmptyMessageStyled } from "./styles/EmptyMessage.styled";
-import { useMemo } from "react";
 import { filterExtensions } from "../utils/extensions";
 
 const CardList = ({ filter }: { filter: string }) => {
   const { extensions } = useExtensions();
-
-  const visibleExtensions = useMemo(
-    () => filterExtensions(filter, extensions),
-    [filter, extensions],
-  );
+  const visibleExtensions = filterExtensions(filter, extensions);
 
   return visibleExtensions.length ? (
     <CardListStyled>
