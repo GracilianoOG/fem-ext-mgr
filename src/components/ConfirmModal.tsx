021e983723f5c81d28cabf44fd5a1b3cda648f49
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-15 10:35:07 -0300

    Extract ConfirmModal styles

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,45 +1,8 @@
-import { popIn } from "../styles/animations";
 import { BorderButtonStyled, DangerButtonStyled } from "./styles/Button.styled";
-import styled from "styled-components";
-
-const ConfirmModalStyled = styled.div`
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.25);
-  display: flex;
-  flex-direction: column;
-  inset: 0;
-  justify-content: center;
-  position: fixed;
-  z-index: 999;
-
-  h2 {
-    color: ${({ theme }) => theme.colors.cardTitleColor};
-    line-height: 2;
-    margin-bottom: 0.5rem;
-  }
-
-  p {
-    color: ${({ theme }) => theme.colors.cardDescColor};
-    line-height: 1.5;
-    margin-bottom: 2rem;
-  }
-
-  & > div {
-    animation: ${popIn} 0.35s forwards ease;
-    box-shadow: ${({ theme }) => theme.boxShadow};
-    background-color: ${({ theme }) => theme.colors.cardBgColor};
-    border-radius: 1.25rem;
-    padding: 1.25rem;
-    width: min(100%, 22.5rem);
-  }
-`;
-
-const ConfirmModalButtonsStyled = styled.div`
-  align-items: center;
-  justify-content: flex-end;
-  display: flex;
-  gap: 8px;
-`;
+import {
+  ConfirmModalButtonsStyled,
+  ConfirmModalStyled,
+} from "./styles/ConfirmModal.styled";
 
 interface ConfirmModalProps {
   isVisible: boolean;
---
+++ src/components/styles/ConfirmModal.styled.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { popIn } from "../../styles/animations";
+
+export const ConfirmModalStyled = styled.div`
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  inset: 0;
+  justify-content: center;
+  position: fixed;
+  z-index: 999;
+
+  h2 {
+    color: ${({ theme }) => theme.colors.cardTitleColor};
+    line-height: 2;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.cardDescColor};
+    line-height: 1.5;
+    margin-bottom: 2rem;
+  }
+
+  & > div {
+    animation: ${popIn} 0.35s forwards ease;
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    background-color: ${({ theme }) => theme.colors.cardBgColor};
+    border-radius: 1.25rem;
+    padding: 1.25rem;
+    width: min(100%, 22.5rem);
+  }
+`;
+
+export const ConfirmModalButtonsStyled = styled.div`
+  align-items: center;
+  justify-content: flex-end;
+  display: flex;
+  gap: 8px;
+`;
