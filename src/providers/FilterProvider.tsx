da3deb5abc58e642fff7152008120682f593cef4
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-15 10:31:56 -0300

    Set filter provider type to FilterOptions

--- src/providers/FilterProvider.tsx
+++ src/providers/FilterProvider.tsx
@@ -1,9 +1,10 @@
 import { useState, type PropsWithChildren } from "react";
 import { FilterContext } from "../contexts/FilterContext";
 import { FilterTypes } from "../enums/FilterTypes";
+import type { FilterOptions } from "../utils/extensions";
 
 const FilterProvider = ({ children }: PropsWithChildren) => {
-  const [filter, setFilter] = useState<string>(FilterTypes.ALL);
+  const [filter, setFilter] = useState<FilterOptions>(FilterTypes.ALL);
 
   return (
     <FilterContext value={{ filter, setFilter }}>{children}</FilterContext>
