de13b28fdd3a213424b890a42196f05597cc250c
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-16 16:56:04 -0300

    Change extension state on animation end

--- src/components/ExtensionToggler.tsx
+++ src/components/ExtensionToggler.tsx
@@ -1,5 +1,6 @@
 import { ExtensionTogglerStyled } from "./styles/ExtensionToggler.styled";
 import { useExtensions } from "../hooks/useExtensions";
+import { useRef, useState } from "react";
 
 interface ExtensionTogglerProps {
   name: string;
@@ -9,18 +10,27 @@ interface ExtensionTogglerProps {
 
 const ExtensionToggler = ({ name, isActive, extId }: ExtensionTogglerProps) => {
   const { setExtensions } = useExtensions();
+  const [isToggled, setIsToggled] = useState(isActive);
+  const clicked = useRef(false);
 
-  const handleClick = () =>
+  const handleAnimationEnd = () => {
+    if (!clicked.current) return;
+
     setExtensions((prevExtensions) =>
       prevExtensions.map((ext) =>
         ext.id === extId ? { ...ext, isActive: !isActive } : ext,
       ),
     );
+  };
 
   return (
     <ExtensionTogglerStyled
-      onClick={handleClick}
-      $isActive={isActive}
+      onClick={() => {
+        setIsToggled(!isToggled);
+        clicked.current = true;
+      }}
+      onAnimationEnd={handleAnimationEnd}
+      $isActive={isToggled}
       role="switch"
       aria-label={`${name} state`}
       aria-checked={isActive}
