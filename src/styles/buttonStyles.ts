d3d8585b9308d8f6b13e8e2beb329c2574d2f960
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-19 12:44:20 -0300

    Apply correct focus effect to danger styled buttons

--- src/styles/buttonStyles.ts
+++ src/styles/buttonStyles.ts
@@ -4,8 +4,10 @@ export const dangerStyle = css`
   background-color: ${({ theme }) => theme.colors.filterBtnBgColorSel};
   color: ${({ theme }) => theme.colors.filterBtnColorSel};
 
+  &:focus-visible,
   &:hover {
     background-color: ${({ theme }) => theme.colors.filterBtnColorSelHover};
+    border-color: ${({ theme }) => theme.colors.filterBtnColorSelHover};
     opacity: 1;
   }
 `;
