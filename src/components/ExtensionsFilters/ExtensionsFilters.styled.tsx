0869c3d6c56bbdaf8b6cc46a9e7eb27af6858cc4
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-19 12:00:51 -0300

    Move extension filters component and styles

--- src/components/ExtensionsFilters/ExtensionsFilters.styled.tsx
+++ src/components/ExtensionsFilters/ExtensionsFilters.styled.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { mediaQueries } from "../../utils/mediaQueries";
+import { mediaQueries } from "@/utils/mediaQueries";
 
 export const ExtensionsFiltersStyled = styled.section`
   align-items: center;
--- src/components/ExtensionsFilters/ExtensionsFilters.tsx
+++ src/components/ExtensionsFilters/ExtensionsFilters.tsx
@@ -1,9 +1,9 @@
-import { ExtensionsFiltersStyled } from "./styles/ExtensionsFilters.styled";
-import { FiltersContainerStyled } from "./styles/FiltersContainer.styled";
-import { TitleStyled } from "./styles/Title.styled";
-import { FilterTypes } from "../enums/FilterTypes";
-import FilterButton from "./FilterButton";
-import { VisuallyHidden } from "./styles/VisuallyHidden.styled";
+import { ExtensionsFiltersStyled } from "./ExtensionsFilters.styled";
+import { FiltersContainerStyled } from "../styles/FiltersContainer.styled";
+import { TitleStyled } from "../styles/Title.styled";
+import { FilterTypes } from "@/enums/FilterTypes";
+import FilterButton from "../FilterButton";
+import { VisuallyHidden } from "../styles/VisuallyHidden.styled";
 
 const ExtensionsFilters = () => {
   return (
